ab634f5d8db3095e464e13880be9dc0d3caa76d6
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-10 23:21:11 -0300

    Wow. I now i have a working autocomplete for python!

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,66 +1,46 @@
-return{
+return {
+
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+--      require("mason").setup()
+    end,
   },
 
   {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-        }
-      })
-    end
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({automatic_installation = true})
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-
-      vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-      vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
-      vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
-
-    end
-  }
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    
+      local servers = { "lua_ls", "jedi_language_server" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          -- on_attach = my_custom_on_attach,
+          capabilities = capabilities,
+        })
+      end
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--      ensure_installed = {
+ --       "lua_ls",
+  --      "pyright",
+ --     },
