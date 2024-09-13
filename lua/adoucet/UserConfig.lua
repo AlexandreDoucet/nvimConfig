b5e7108a8089da0fcd97a896de30fa0a31cd0f80
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-12 21:30:49 -0300

    Added git signs

--- lua/adoucet/UserConfig.lua
+++ lua/adoucet/UserConfig.lua
@@ -11,6 +11,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+
+
 -- Editor parameters
 vim.opt.ttimeoutlen = 10 -- helps with registering leader key and escape faster (ToTest)
 vim.opt.relativenumber = true -- Uses relative numbers for easier jumping
@@ -19,6 +21,7 @@ vim.opt.number = true -- Show the current line number the cursor is on instead o
 vim.opt.scrolloff = 10 -- Keeps cursor above N lines of the enf of the screen if not at the end of the file
 vim.opt.undofile = true --preserve undo after close (ToTest)
 
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -26,3 +29,26 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
