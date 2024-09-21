3e8f08123a58b0863cc3422bb6d8225932407716
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-21 18:35:18 -0300

    .

--- lua/plugins/Debug/debuggin_configs.lua
+++ lua/plugins/Debug/debuggin_configs.lua
@@ -1,51 +1,50 @@
 return {
 
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
-		},
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
 
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
       require("dapui").setup()
-      
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			vim.keymap.set("n", "<F5>", dap.continue, {})
-			vim.keymap.set("n", "<F10>", dap.step_over, {})
-			vim.keymap.set("n", "<F11>", dap.step_into, {})
-			vim.keymap.set("n", "<F12>", dap.step_out, {})
-			vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, {})
-		end,
-	},{
-    
+      vim.keymap.set("n", "<F5>", dap.continue, {})
+      vim.keymap.set("n", "<F10>", dap.step_over, {})
+      vim.keymap.set("n", "<F11>", dap.step_into, {})
+      vim.keymap.set("n", "<F12>", dap.step_out, {})
+      vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, {})
+
+    end,
+  },
+  {
+
     "jay-babu/mason-nvim-dap.nvim",
     event = "VeryLazy",
     dependencies = {
-		"mfussenegger/nvim-dap",
-    "williamboman/mason.nvim"
-
+      "mfussenegger/nvim-dap",
+      "williamboman/mason.nvim",
     },
     opts = {
       handlers = {},
-      ensure_installed = {'codelldb'}
-    }
-
-  }
+      ensure_installed = { "codelldb","python" },
+    },
+  },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,11 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-
+  close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+  popup_border_style = "rounded",
   config = function()
+
+
+
+
     --vim.keymap.set('n','<leader>n', ':Neotree filesystem toggle left<CR>') -- toggles the file system visibility
---    vim.keymap.set("n", "<leader><leader>", ":Neotree filesystem toggle show left<CR>") -- toggles the file system visibility
-    vim.keymap.set("n", "<leader>n", ":Neotree filesystem focus left<CR>")            -- toggles the file system visibility
-    vim.keymap.set("n", "<leader>m", ":Neotree filesystem close left<CR>")            -- toggles the file system visibility
+    --    vim.keymap.set("n", "<leader><leader>", ":Neotree filesystem toggle show left<CR>") -- toggles the file system visibility
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem focus left reveal<CR>") -- toggles the file system visibility
+    vim.keymap.set("n", "<leader>m", ":Neotree filesystem close left<CR>") -- toggles the file system visibility
   end,
 }
