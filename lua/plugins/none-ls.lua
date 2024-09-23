cb84b38ecb65ffcd79c2db607d2058bc1ac7170c
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-22 23:05:06 -0300

    Correcly ? fixed the mason loading by finding a typo in dependencies. Changes None_ls to add somme error checking.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,27 +1,46 @@
 return {
-
-	"nvimtools/none-ls.nvim",
+	"nvimtools/none-ls.nvim",  
 
 	config = function()
 		local null_ls = require("null-ls")
+
+		-- Define formatting sources
+		local formatting_sources = {
+			-- Lua formatting using stylua
+			null_ls.builtins.formatting.stylua,
+
+			-- Python formatting using black and isort
+			null_ls.builtins.formatting.black,
+			null_ls.builtins.formatting.isort,
+
+			-- C++ formatting using clang_format
+			null_ls.builtins.formatting.clang_format,
+
+		}
+
+		-- Function to check if required tools are installed
+		local function check_sources(sources)
+			for _, source in ipairs(sources) do
+				local name = source._name or source.name
+				local command = source._command or source.command  -- Get command to check
+
+				if command and vim.fn.executable(command) == 0 then
+					vim.notify(name .. " is not installed.", vim.log.levels.ERROR)
+				end
+			end
+		end
+
+		-- Check if any required tools are missing
+		check_sources(formatting_sources)
+
+		-- Setup null-ls with the defined sources
 		null_ls.setup({
-			sources = {
-        --lua
-				null_ls.builtins.formatting.stylua,
-
-        --python
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-
-        --c++
-        null_ls.builtins.formatting.clang_format,
-
-        --rust
---        null_ls.builtins.formatting.dxfmt
-
-
-			},
+			sources = formatting_sources,
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+		-- Key mapping to format the current buffer
+		vim.keymap.set("n", "<leader>gf", function()
+			vim.lsp.buf.format({ async = true })  -- Use async formatting
+		end, { silent = true, desc = "Format current buffer" })
 	end,
 }
