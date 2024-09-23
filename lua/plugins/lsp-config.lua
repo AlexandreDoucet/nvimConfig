bf0e0d762b0bf546db2966458f35e588e0554cef
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-22 22:25:11 -0300

    Added Mason as a requirement and made the rust lsp lazy load to remove a race condition.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,63 +1,68 @@
 return {
-	{
-		"williamboman/mason.nvim",
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependecies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    lazy = false,
+    config = function()
+      local servers = { "lua_ls", "jedi_language_server", "clangd" }
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			automatic_installation = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependecies = {},
-		lazy = false,
-		config = function()
-			local servers = { "lua_ls", "jedi_language_server", "clangd" }
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      require("mason").setup()
+      require("mason-lspconfig").setup({ ensure_installed = servers })
 
-			require("mason").setup()
-			require("mason-lspconfig").setup({ ensure_installed = servers })
+      local on_attach = function()
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end
 
-			local on_attach = function()
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			end
+      for _, lsp in ipairs(servers) do
+        if lsp == "rust_analyzer" then
+        end
 
-			for _, lsp in ipairs(servers) do
-				if lsp == "rust_analyzer" then
-				end
+        lspconfig[lsp].setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      end
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = true, -- This plugin is already lazy
+    dependecies = {
 
-				lspconfig[lsp].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end
-		end,
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false, -- This plugin is already lazy
-		config = function()
-			local mason_registry = require("mason-registry")
-			local codelldb = mason_registry.get_package("codelldb")
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			local cfg = require("rustaceanvim.config")
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local mason_registry = require("mason-registry")
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      local cfg = require("rustaceanvim.config")
 
-			vim.g.rustaceanvim = {
-				dap = {
-					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-				},
-			}
-		end,
-	},
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end,
+  },
 }
