f155960eba6941178f3721439d3eb1f1a903644f
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-09-28 13:00:23 -0300

    Added a codelldb check

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,75 +1,78 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-		lazy = false,
-		opts = {
-			automatic_installation = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		lazy = false,
-		config = function()
-			local servers = { "lua_ls", "jedi_language_server", "clangd" }
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    lazy = false,
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    lazy = false,
+    config = function()
+      local servers = { "lua_ls", "jedi_language_server", "clangd" }
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			require("mason").setup()
-			require("mason-lspconfig").setup({ ensure_installed = servers })
+      require("mason").setup()
+      require("mason-lspconfig").setup({ ensure_installed = servers })
 
-			local on_attach = function()
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			end
+      local on_attach = function()
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end
 
-			for _, lsp in ipairs(servers) do
-				if lsp == "rust_analyzer" then
-				end
+      for _, lsp in ipairs(servers) do
+        if lsp == "rust_analyzer" then
+        end
 
-				lspconfig[lsp].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end
-		end,
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false,
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"williamboman/mason.nvim", -- Ensure this is loaded first
+        lspconfig[lsp].setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      end
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim", -- Ensure this is loaded first
       "jay-babu/mason-nvim-dap.nvim",
-		},
-		config = function()
+    },
+    config = function()
       print("rustaceanvim function is running")
-			local mason_registry = require("mason-registry")
-			local codelldb = mason_registry.get_package("codelldb")
-			if not codelldb then
-				vim.notify("codelldb package not found, make sure it's installed with Mason", vim.log.levels.ERROR)
-				return
-			end
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			local cfg = require("rustaceanvim.config")
+      local mason_registry = require("mason-registry")
+      local has_codelldb = mason_registry.has_package("codelldb")
+      if has_codelldb then
+        local codelldb = mason_registry.get_package("codelldb")
+        if not codelldb then
+          vim.notify("codelldb package not found, make sure it's installed with Mason", vim.log.levels.ERROR)
+          return
+        end
+      end
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      local cfg = require("rustaceanvim.config")
 
-			vim.g.rustaceanvim = {
-				dap = {
-					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-				},
-			}
-		end,
-	},
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end,
+  },
 }
