0780ecda8aa6370ef9d9e8110fc96fe6d93f2bcd
Alexandre Doucet <alexandre.doucet6@gmail.com>
2024-10-05 23:05:12 -0300

    - Ensured that the recording macro notifcation was on the lualine bar.  _ Modified hidden files in neotree.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,22 @@
-return
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup({
-        options = {
-          theme = 'dracula'
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
 
-        }
-      })
-    end
-  }
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+
+			options = {
+				theme = "dracula",
+			},
+		})
+	end,
+}
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,16 +6,46 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+
   close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-  popup_border_style = "rounded",
-  config = function()
 
+  opts = {
 
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          --"node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        always_show_by_pattern = { -- uses glob style patterns
+          --".env*",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          --".DS_Store",
+          --"thumbs.db"
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
+    },
+  },
 
-
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
     --vim.keymap.set('n','<leader>n', ':Neotree filesystem toggle left<CR>') -- toggles the file system visibility
     --    vim.keymap.set("n", "<leader><leader>", ":Neotree filesystem toggle show left<CR>") -- toggles the file system visibility
     vim.keymap.set("n", "<leader>n", ":Neotree filesystem focus left reveal<CR>") -- toggles the file system visibility
-    vim.keymap.set("n", "<leader>m", ":Neotree filesystem close left<CR>") -- toggles the file system visibility
+    vim.keymap.set("n", "<leader>m", ":Neotree filesystem close left<CR>")      -- toggles the file system visibility
   end,
 }
